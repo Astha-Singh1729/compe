5479962790f4a58f4d8b10faeb2e4f9dc0542b98
Astha Singh <asthasingh1729@gmail.com>
2022-04-03 10:31:23 +0530

    fixed

--- data_structures/segtree_class.cpp
+++ data_structures/segtree_class.cpp
@@ -1,4 +1,3 @@
-/* someone remind me , need to fix few stuff in this in future*/
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
@@ -8,58 +7,58 @@ class segU
     {
         int value;
     };
+    int n;
+    vector<element> t;
+    element null = {0};
+
+    void point_update(int v, int l, int r, element value, int pos)
+    {
+
+        if (r - l == 1)
+        {
+            t[v] = value;
+            return;
+        }
+
+        int mid = (l + r) / 2;
+        if (pos < mid)
+            point_update(v * 2, l, mid, value, pos);
+        else
+            point_update(v * 2 + 1, mid, r, value, pos);
+        t[v] = merge(t[2 * v], t[2 * v + 1]);
+    }
+
+    element ans_chahiye(int l, int r, int ql, int qr, int v)
+    {
+        if (l >= ql && r <= qr)
+            return t[v];
+        if (r <= ql || qr <= l)
+            return null;
+        int mid = (l + r) / 2;
+        return merge(ans_chahiye(l, mid, ql, qr, v * 2), ans_chahiye(mid, r, ql, qr, v * 2 + 1));
+    }
+
+    element merge(element a, element b)
+    {
+        element c;
+        c.value = a.value + b.value;
+        return c;
+    }
 
 public:
     segU(int n)
     {
-        this->t.resize(4 * n, this->null);
         this->n = n;
+        t.resize(4 * n, null);
     }
 
-    element prayge(int l, int r)
+    void point_update(int pos, int value)
     {
-        return prayge(1, 0, n - 1, l, r);
+        point_update(1, 0, n, {value}, pos);
     }
 
-    void update(int pos, int value)
+    element ans_chahiye(int ql, int qr)
     {
-        update(1, 0, n - 1, pos, value);
-    }
-
-private:
-    vector<element> t;
-    int n;
-    element null = {0};
-    element prayge(int v, int tl, int tr, int l, int r)
-    {
-        if (l > r)
-            return null;
-        if (l == tl && r == tr)
-        {
-            return t[v];
-        }
-        int tm = (tl + tr) / 2;
-        return merge(prayge(v * 2, tl, tm, l, min(r, tm)), prayge(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r));
-    }
-    void update(int v, int tl, int tr, int pos, int new_val)
-    {
-        if (tl == tr)
-        {
-            t[v].value = new_val;
-        }
-        else
-        {
-            int tm = (tl + tr) / 2;
-            if (pos <= tm)
-                update(v * 2, tl, tm, pos, new_val);
-            else
-                update(v * 2 + 1, tm + 1, tr, pos, new_val);
-            t[v] = merge(t[v * 2], t[v * 2 + 1]);
-        }
-    }
-    element merge(element l, element r)
-    {
-        element ans = {l.value + r.value};
-        return ans;
+        return ans_chahiye(0, n, ql, qr, 1);
     }
 };
